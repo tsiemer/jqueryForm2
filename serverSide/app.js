b6f09d9571869001d7370aa8784c9554464cd398
Trinity Siemer <trinitysiemer@TrinitysMacBook.attlocal.net>
2017-08-22 20:36:06 -0500

    https://github.com/tsiemer/jqueryForm2.git

--- serverSide/app.js
+++ serverSide/app.js
@@ -5,7 +5,7 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-var people = [];
+var employees = [{"firstname":"Trinity","lastname":"Siemer"}];
 var currentId = 0;
 
 function Employee(req,res, next){
@@ -14,14 +14,26 @@ function Employee(req,res, next){
 }
 
 function getEmployees(req, res, next){
-	res.send(people);
+	res.send(employees);
 }
 
+function postEmployee(req,res, next){
+	var employee = new Employee();
+
+	employee.firstname = req.body.firstname;
+	employee.lastname = req.body.lastname;
+
+	res.send(employee);
+	console.log(employee)
+}
 function getEmployee(req, res, next){
 	
 }
 
 server.get('/people', getEmployees);
+server.get('/people/:id', getEmployee);
+server.post('/people', postEmployee);
+
 server.listen(3001, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
